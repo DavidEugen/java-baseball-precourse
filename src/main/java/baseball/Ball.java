ddd57733a8e996921ed436b20af7bdc846d7afd6
DavidEugen <platonicojju@gmail.com>
2022-04-13 23:53:10 +0900

    test: 볼 객체 비교 판단
    볼 1구가 스트라이크인지 판단 ( 같은 자리, 같은 수)

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -1,7 +1,31 @@
 package baseball;
 
 public class Ball {
+
+    int position;
+    int number;
+
+    public Ball(int position, int number) {
+        this.position = position;
+        this.number = number;
+    }
+
     public static boolean isInRangeNumber(int number) {
         return number > 0 && number < 10;
+    }
+
+    public String compareBall(Ball oppositeBall) {
+        if(oppositeBall.getPosition() == position && oppositeBall.getNumber() == number){
+            return "Strike";
+        }
+        return "ball or nothing";
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getNumber() {
+        return number;
     }
 }
--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -1,8 +1,10 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -12,8 +14,21 @@ public class BallTest {
     @DisplayName("1~9까지 유효 범위 내의 숫자 인지 테스트")
     @CsvSource(value = {"-1,false", "0,false", "1,true", "9,true", "10,false"})
     void 숫자_생성_검증(int inputNumber, boolean expected) {
+        //when
         boolean rightNumber = Ball.isInRangeNumber(inputNumber);
+        //then
         assertThat(rightNumber).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("스트라이크 여부 테스트")
+    void 스트라이크_여부_테스트(){
+        //given
+        Ball computer = new Ball(1, 3);
+        //when
+        String result = computer.compareBall(new Ball(1, 3));
+        //then
+        assertThat(result).isEqualTo("Strike");
     }
 
 }
